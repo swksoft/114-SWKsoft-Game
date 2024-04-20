ae67116df668d49840c8645470c494abcf3feb33
swksoft <esparzapatricio95@gmail.com>
2024-04-20 20:51:19 -0300

    game over 2

--- scripts/game_over.gd
+++ scripts/game_over.gd
@@ -1,19 +1,42 @@
 extends Area2D
 
+# TODO: tienen que aparecer grietas como si se rompiera
+
+@export var minimum_force_break = 15
+@export var crack_scene : PackedScene
+
 @onready var mirror_health: int = 300
 @onready var canvas_layer = $CanvasLayer
 @onready var timer = $Timer
 @onready var anim = $AnimationPlayer
+@onready var animated_sprite_2d = $AnimatedSprite2D
+
+func _ready():
+	animated_sprite_2d.frame = 0
 
 func check_health():
+	if mirror_health < 250 and mirror_health > 200:
+		animated_sprite_2d.frame = 1
+	elif mirror_health < 199 and mirror_health > 75:
+		animated_sprite_2d.frame = 2
+	elif mirror_health < 74:
+		animated_sprite_2d.frame = 3
+		
+	
 	if mirror_health <= 0:
 		timer.start()
 		anim.play("appear")
 
 func crack(force):
-	mirror_health -= force
-	print("salud espejo: ", mirror_health)
-	check_health()
+	var crack = crack_scene.instantiate()
+	crack.global_position = get_global_mouse_position()
+	
+	if force >= minimum_force_break:
+		mirror_health -= force
+		print("salud espejo: ", mirror_health)
+		add_child(crack)
+		check_health()
+		
 
 func _on_timer_timeout():
 	get_tree().change_scene_to_file("res://scenes/game_scene.tscn")
